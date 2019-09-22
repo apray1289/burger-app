a06de8e62e0b081d353021b51caeb4bf4951941d
Andrew Pray <a.pray1289@gmail.com>
2019-09-22 18:38:51 -0500

    gitignore

---
+++ public/assets/js/burgersAssets.js
@@ -0,0 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var devourThat = $(this).data("devourthat");
+
+    var eatenState = {
+      devoured: devourThat
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eatenState
+    }).then(
+      function() {
+        
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#bu").val().trim(),
+      devoured: 0
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
