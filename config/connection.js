506b935648164655b091b6f24d8f25599fcdd18a
Andrew Pray <a.pray1289@gmail.com>
2019-09-22 18:57:24 -0500

    added Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
-
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
@@ -17,6 +20,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
 
 module.exports = connection;
